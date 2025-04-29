7c8876a902749c6246c892f74f7e7aca12d17452
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-04-29 18:31:24 -0300

    Add RootLayout component with GestureHandler and Drawer

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+
+export default function RootLayout() {
+    return (
+        <GestureHandlerRootView>
+            <Drawer>
+                
+            </Drawer>
+        </GestureHandlerRootView>
+    );
+}
